8f69ec61d1bd985778bab63050c501a851241ffc
Rommel B. Mabini Jr <rommelbrillantesmabinijr@gmail.com>
2022-02-24 13:39:19 +0800

    add operate function & return the function for the operator

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 
+function operate(operator, a, b) {
+
+    switch(operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+        default:
+            return "Not a valid operation!";
+      }
+}
+
+
+
 function add(a, b) {
     return a + b;
 }
