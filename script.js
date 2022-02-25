cb8cb58a68c2bf3dec1b9fcef4773e78163caef6
Rommel B. Mabini Jr <rommelbrillantesmabinijr@gmail.com>
2022-02-25 17:04:42 +0800

    optimized the event listeners for buttons and create function that calculates the first set of operation

--- script.js
+++ script.js
@@ -1,11 +1,76 @@
 let displayCon = document.querySelector(".display")
 const numBtns = document.querySelectorAll(".digit-con button")
+const operators = document.querySelectorAll(".operations-con button")
+let firstNum, secondNum, operatorUsed;
+let allowOperate = false;
+let tempNum = 0;
 
 numBtns.forEach( button => {
-    button.addEventListener('click', function(e) {
-        displayCon.textContent += e.target.textContent;
-    })
+    button.addEventListener('click', enterOperand)
 })
+
+operators.forEach(button => {
+    button.addEventListener('click', enterOperator);
+})
+
+
+
+
+
+
+
+
+function enterOperand(num) {
+    let number = num.target.textContent
+    displayCon.textContent += number;
+    tempNum += number;
+
+    //Once we enter number, operand buttons are allowed
+    allowOperate = true
+}
+
+function enterOperator(operator) {
+
+    //this simply limits the operand entered one at a time unless a number is entered.
+    if (allowOperate) {
+
+        if (operator.target.textContent == '=') {
+
+            //assuming that firsNum is already set, change num values to integers
+            secondNum = tempNum;
+            
+            startCalculate();
+            allowOperate = false
+
+
+
+
+        } else {
+
+            //automatically calculates unfinished operation if operand is clicked instead of equal sign
+            if (operatorUsed) {
+                secondNum = tempNum;
+                startCalculate();
+            }
+
+            firstNum = tempNum;
+            tempNum = "";
+            operatorUsed = operator.target.textContent;
+            displayCon.textContent += operator.target.textContent;
+            allowOperate = false;
+        }
+    }
+}
+
+function startCalculate() {
+    firstNum = Number(firstNum);
+    secondNum = Number(secondNum);
+
+    displayCon.textContent = operate(operatorUsed, firstNum, secondNum);
+
+    //assign the result in the temp var to be automatically assigned as firstNum later on
+    tempNum = displayCon.textContent;
+}
 
 function operate(operator, a, b) {
 
