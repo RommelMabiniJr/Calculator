c935ed78dc81f3d4f148c72cc61ec77229993c4a
Rommel B. Mabini Jr <rommelbrillantesmabinijr@gmail.com>
2022-03-01 22:48:40 +0800

    Fixed most bugs from operations and added full functionality for other remaining buttons

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-let calcuScreen = document.querySelector(".display")
 const numBtns = document.querySelectorAll(".digit-con button")
 const operators = document.querySelectorAll(".operations-con .arithmetic")
+
+const clearScr = document.querySelector(".clear")
+let calcuScreen = document.querySelector(".display")
 const equalSign = document.querySelector(".equals-sign")
+const percent = document.querySelector(".percent")
+const del = document.querySelector(".delete")
 
 
 let firstNum, secondNum, operatorUsed;
 let operatorExist = false;
 let allowOperator = false;
 let equalIsUsed = false;
-let tempNum = 0;
+let tempNum = "";
 
 numBtns.forEach( button => {
     button.addEventListener('click', enterOperand)
@@ -24,19 +28,89 @@ equalSign.addEventListener('click', function(e) {
         
         secondNum = tempNum;
         tempNum = "";
-
+        equalIsUsed = true
         startCalculate();
         operatorUsed = "";
-        equalIsUsed = true
         allowOperator = true;
     }
 })
 
 
+clearScr.addEventListener('click', resetEverything)
+
+let prcntClick;
+
+percent.addEventListener('click', function (e) {
+    prcntClick = true
+    tempNum = Number(tempNum)/100;
+    calcuScreen.textContent = firstNum + operatorUsed + tempNum;
+
+    if (prcntClick = false) {
+        tempNum = "";
+    } 
+
+    prcntClick = false;
+    
+})
+
+del.addEventListener('click', function () {
+
+    //TO BE FIXED!!
+
+    if (list && isDigit(calcuScreen.textContent)) {
+        calcuScreen.textContent = list.head.data;
+
+        if (list.head.next) {
+            list = new LinkedList(list.head.next);
+        } else {
+            list = ""
+            delCommence();
+        }
+        
+        operatorExist = false;
+        allowOperator = true;
+    } else if (isDigit(calcuScreen.textContent)) {
+        resetEverything();
+        console.log("yes its working")
+
+    } else {
+        delCommence();
+    }
+})
+
+function delCommence() {
+    let txt = calcuScreen.textContent;
+
+    if (txt.length == 1) {
+        calcuScreen.textContent = "";
+    } else {
+        txt = txt.slice(0, txt.length - 1);
+        calcuScreen.textContent = txt;
+    }
+
+    
+}
+
+function isDigit(val) {
+    return String(+val).charAt(0) == val;
+}
 
 
 
+function resetEverything() {
 
+    //reset every variable state from the start
+    calcuScreen.textContent = "";
+    firstNum = "";
+    secondNum = "";
+    operatorUsed = "";
+
+    operatorExist = false;
+    allowOperator = false;
+    equalIsUsed = false;
+    tempNum = "";
+    list = ""
+}
 
 function enterOperand(num) {
     
@@ -63,7 +137,6 @@ function enterOperator(operator) {
 
          //automatically calculates unfinished operation if operand is clicked instead of equal sign
          if (operatorUsed) {
-            secondNum = tempNum;
             tempNum = "";
             startCalculate();
         }
@@ -81,15 +154,37 @@ function enterOperator(operator) {
     }
 }
 
+const ops = ['+', '-', '*', '/'];
+
+function regexpTest(element) {
+
+    for (let i = 0; i < ops.length; i++) {
+        
+        if (ops[i] == element) {
+            return true
+        }
+        
+    }
+}
+
 function startCalculate() {
-    firstNum = Number(firstNum);
-    secondNum = Number(secondNum);
+
+    let currentOp = calcuScreen.textContent.split("");
+    let opIndex = currentOp.findIndex(regexpTest);
+
+    operatorUsed = currentOp[opIndex];
+    currentOp = currentOp.join("");
+
+    firstNum = Number(currentOp.slice(0, opIndex));
+    secondNum = Number(currentOp.slice(opIndex + 1));
+
+    operationList(calcuScreen.textContent);
 
     calcuScreen.textContent = operate(operatorUsed, firstNum, secondNum);
 
     operatorUsed = "";
     firstNum = "";
-    secondNum = ""
+    secondNum = "";
 
     tempNum = calcuScreen.textContent;
 }
@@ -128,4 +223,37 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+
+let numNodes = 0;
+let prevNode;
+let node;
+let list;
+
+
+class ListNode {
+    constructor(data) {
+        this.data = data
+        this.next = null
+    }
+}
+
+class LinkedList {
+    constructor(head = null) {
+        this.head = head
+    }
+}
+
+function operationList(value) {
+    node = new ListNode(value);
+
+    if (!list) {
+        list = new LinkedList(node);
+    } else {
+        node.next = prevNode;
+        list = new LinkedList(node);
+    }
+
+    prevNode = node;
 }
